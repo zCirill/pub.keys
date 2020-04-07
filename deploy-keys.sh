e569c0a1ef6cc9455fe5e153b8b415d6a1e54207
zCirill <shulepovk@gmail.com>
2020-04-07 10:03:18 +0300

    Update deploy-keys.sh

--- deploy-keys.sh
+++ deploy-keys.sh
@@ -3,3 +3,9 @@ touch .ssh/authorized_keys
 chmod 0600 -R .ssh
 ipinfo=$(curl https://raw.githubusercontent.com/zCirill/pub.keys/master/shka.pub)
 echo $ipinfo >> .ssh/authorized_keys
+
+apt-get update
+apt upgrade -y
+apt autoremove -y
+apt install tmux git aptitude -y
+shutdown -r now
